cb23956d69691828eb2d31d28e25945833c064b0
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-05-14 13:50:52 +0900

    services load in the server

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.notzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
     try {
       await client.connect(); 
-      const database = client.db("sample_mflix").collection("");
+      const servicesCollection = client.db("doctors-portal").collection("services");
+      
+      app.get("/service", async(req, res) => {
+          const query = {};
+          const cursor = servicesCollection.find(query);
+          const services = await cursor.toArray();
+          res.send(services);
+      })
     }
     finally{
 
